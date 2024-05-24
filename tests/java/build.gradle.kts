54d4e7050e6a24fccc1133ec0dac16d844e1abc5
Staeck, Philipp <philipp.staeck@draeger.com>
2024-05-24 14:33:36 +0200

    use GITHUB_RUN_NUMBER

--- tests/java/build.gradle.kts
+++ tests/java/build.gradle.kts
@@ -14,9 +14,10 @@ val configFileMap = configFile.associate { it.split("=")[0] to it.split("=")[1] 
 
 val grpcVersion = configFileMap["JAVA_GRPC_VERSION"]
 val baseVersion = configFileMap["BASE_PACKAGE_VERSION"]!!
+val buildId: String? = System.getenv("GITHUB_RUN_NUMBER")
 val t2iapiVersion: String = when (System.getenv("RELEASE_VERSION") == "1") {
     true -> baseVersion
-    false -> baseVersion + "-SNAPSHOT"
+    false -> baseVersion + ( buildId?.let { ".$it" } ?: "" ) + "-SNAPSHOT"
 }
 
 dependencies {
